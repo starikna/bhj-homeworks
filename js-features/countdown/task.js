5dba98f0dedb62e24a253ebf493774ae5dab854a
starikna <don.strekoza@gmail.com>
2022-11-10 18:30:52 +0500

    First commit

---
+++ js-features/countdown/task.js
@@ -0,0 +1,19 @@
+`use strict`
+
+const reverseTimer = document.getElementById("timer");
+
+const startingPoint = "60";
+
+const getReverseTimer = function () {
+if (reverseTimer.textContent === "0") {
+    alert("Вы победили в конкурсе");
+
+    return (reverseTimer.textContent = startingPoint);
+  } else {
+  reverseTimer.textContent--;
+  }
+}
+
+    setInterval(getReverseTimer, 1000);
+
+
