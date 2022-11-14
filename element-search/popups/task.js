f83dabe8c9b240d202b96c3221571dc6a9f229f5
starikna <don.strekoza@gmail.com>
2022-11-14 17:03:53 +0500

    First commit

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,22 +1,16 @@
 const modalWindow = window.modal_main; 
 const oldClassModal = modalWindow.className;
 modalWindow.className = "modal modal_active";
-
-// Сделайте закрытие активного окна по нажатию на его элемент с классом modal__close
-
-
-    
+  
 const closeWindow = document.getElementsByClassName("modal__close");
 
 closeWindow[0].onclick = function () {
     modalWindow.className = "modal";
 }
 
-// По нажатию на элемент с классом show-success покажите окно #modal_success
-
     const modalSuccess = window.modal_success; 
     const oldClassSuccess = modalSuccess.className;
-    
+
 closeWindow[1].onclick = function () {
     
     modalWindow.className = "modal";
@@ -24,7 +18,6 @@ closeWindow[1].onclick = function () {
 }
 
 closeWindow[2].onclick = function () {
-    // const modalSuccess = window.modal_success; 
-    // const oldClassSuccess = modalSuccess.className;
+
     modalSuccess.className = "modal";
 }
