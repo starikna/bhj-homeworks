3e76a94accbd9b691b08681fe638f31ce92d17ce
starikna <don.strekoza@gmail.com>
2022-11-19 18:32:18 +0500

    First commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,30 +1,22 @@
 // // Используя цикл зарегистрируйте обработчики события click на элементах с классом menu__link
 const link = Array.from(document.querySelectorAll(".menu__link"));
-const menuSub = Array.from(document.querySelectorAll(".menu_sub"));
+const menuSubAll = Array.from(document.querySelectorAll(".menu_sub"));
 
 link.forEach((el) => {
   el.onclick = function () {
-     menuSub.find((item) => {
-if(el.nextElementSibling === item) {
-         item.classList.toggle("menu_active"); 
-         return false;  
-} 
-
+    menuSubAll.find((item) => {
+      if(el.nextElementSibling === item) {
+        item.classList.toggle("menu_active"); 
+      } 
     });
     return false;   
   } 
-//   return false;
 });
-
-// console.log(link);
-// console.log(item.menuSubALL);
-// console.log(menuSub);
 
 
 // Найдите меню рядом со ссылкой. Если оно есть, переключите у него класс menu_active
 // Запрещайте переход по ссылке для тех, что имеют вложенное меню. Остальные пункты меню должны
 //  без помех переводить пользователя на соответствующие страницы.
-
 
 // Используемые темы
 // Событие click, метод onclick, обработчик события
