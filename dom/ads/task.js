234e67584c818804fec629ac940d28fb114d31b8
starikna <don.strekoza@gmail.com>
2022-12-20 19:20:57 +0500

    First commit

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,7 +1,53 @@
-const rotator = document.querySelectorAll(".rotator__case");
-console.log(rotator);
-rotator.forEach((el) => {
+
 function changeRotator () {
-    el.classList.toggle("rotator__case_active")
+    const rotatorArr = Array.from(document.querySelectorAll(".rotator__case"));
+
+       let itemActive = document.querySelector(".rotator__case_active");
+       console.log(itemActive);
+       
+       let i = rotatorArr.findIndex((item) => item === itemActive);
+       console.log(i);
+
+       rotatorArr[i].classList.toggle("rotator__case_active");
+       let countItem = ++i;
+       console.log(countItem);
+
+       (countItem < rotatorArr.length) ?
+       i = countItem :
+       i = countItem - rotatorArr.length; 
+       
+       rotatorArr[i].classList.toggle("rotator__case_active");
+       
 }
-});
+
+setInterval(() => {
+    changeRotator();
+}, 1000);
+
+// __________________________________________________________________________________
+// class Banner {
+//   constructor(container) {
+//     this.container = container;
+//     this.rotator = container.querySelector('.rotator');
+//     this.cases = this.rotator.children;
+//     this.currentCase = this.rotator.querySelector('.rotator__case_active');
+//     this.currentCaseIndex = Array.from(this.cases).indexOf(this.currentCase);
+    
+//     this.rotate = this.spanRotator.bind(this);
+    
+//     this.interval = setInterval(this.rotate, 1000);
+//   }
+
+//   spanRotator() {
+//     this.cases[this.currentCaseIndex].classList.remove('rotator__case_active');
+    
+//     this.currentCaseIndex++;
+//     if (this.currentCaseIndex > this.cases.length - 1) {
+//       this.currentCaseIndex = 0;
+//     }
+//     this.cases[this.currentCaseIndex].classList.add('rotator__case_active');    
+    
+//   }
+// } 
+
+// document.querySelectorAll('p').forEach(banner => new Banner(banner));
---
+++ dom/book-reader/task.js
@@ -0,0 +1,20 @@
+const books = document.querySelectorAll(".book");
+books.forEach((book) => {
+  book.addEventListener("click", (e) => {
+    const currentLink = e.target;
+    if (!currentLink.classList.contains("font-size")) return;
+    if (currentLink.classList.contains("font-size_active")) return;
+    e.preventDefault();
+    
+    const currentLinkSize = currentLink.getAttribute("data-size");
+    const currentBook = currentLink.closest(".book");
+   
+    let activeItem = currentBook.querySelector(".font-size_active");
+    currentLink.classList.add("font-size_active");
+    activeItem.classList.remove("font-size_active");
+    activeItem = currentBook.querySelector(".font-size_active");
+    
+    currentBook.className = "book";
+    currentBook.classList.add(`book_fs-${currentLinkSize}`);
+  })
+});
